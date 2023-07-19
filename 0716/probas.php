2ce2567ee4d7bdc968e21e3d68170974d508c559
artistic software <artisticsoftware41@gmail.com>
2023-07-19 05:15:47 +0000

    ...

--- 0716/probas.php
+++ 0716/probas.php
@@ -6,77 +6,76 @@ return [
 		Race 1
 		*/
 		'qpl' => '5 X 3, 6',
-		'PLACE' => 'NO',
+		//If qpl payout > 100 HKD.
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'qpl' => '2 X 5, 7, 8, 10, 11',
-		'PLACE' => '2',
+		//If qpl payout > 50 HKD.
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'qpl' => '1 X 3, 4, 6, 8, 12',
-		'PLACE' => '1',
+		//If qpl payout > 50 HKD.
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'qpl' => '4 X 1, 2, 10',
-		'PLACE' => '4',
+		//If qpl payout > 50 HKD.
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'qpl' => '9 X 2, 3, 4, 5, 7, 12',
-		'PLACE' => 'NO',
+		//If qpl payout > 100 HKD.
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'qpl' => '1 X 2, 3, 4, 6, 9, 13',
-		'PLACE' => '1',
+		//If qpl payout > 50 HKD.
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'qpl' => '14 X 1, 12',
-		'PLACE' => '14',
+		//If qpl payout > 50 HKD.
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'qpl' => '2 X 3, 4, 5',
-		'PLACE' => 'NO',
+		//If qpl payout > 100 HKD.
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'qpl' => '1 X 12',
-		'Win' => '1',
-		'PLACE' => '1',
+		//If qpl payout > 50 HKD.
 	],
 	'10' => [
 		/**
 		Race 10
 		*/
 		'qpl' => '4 X 3, 6, 9, 11',
-		'PLACE' => '4',
+		//If qpl payout > 50 HKD.
 	],
 	'11' => [
 		/**
 		Race 11
 		*/
 		'qpl' => '2 X 4, 10, 13',
-		'PLACE' => '2',
+		//If qpl payout > 50 HKD.
 	],
 ];

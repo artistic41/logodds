b581b4798d2d30580f8a4879082766e6cd5d1e6c
artistic software <artisticsoftware41@gmail.com>
2023-07-19 05:17:25 +0000

    ...

--- archive/0712/probas.php
+++ archive/0712/probas.php
@@ -6,54 +6,62 @@ return [
 		Race 1
 		*/
 		'qpl' => '2 X 3, 5',
+		//If qpl payout > 50 HKD.
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
 		'qpl' => '4 X 2',
-		'Win' => '4',
+		//If qpl payout > 50 HKD.
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
 		'qpl' => '5 X 2, 8, 11, 12',
+		//If qpl payout > 50 HKD.
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'qpl' => '6 X 2, 5, 8, 11, 12',
+		//If qpl payout > 50 HKD.
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'qpl' => '6 X 2, 12',
+		//If qpl payout > 50 HKD.
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'qpl' => '4 X 1, 2, 6, 11, 12',
+		//If qpl payout > 50 HKD.
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'qpl' => '5 X 3, 7, 11, 12',
+		//If qpl payout > 50 HKD.
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'qpl' => '3 X 2, 5, 11',
+		//If qpl payout > 50 HKD.
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'qpl' => '5 X 2, 7, 11',
+		//If qpl payout > 50 HKD.
 	],
 ];

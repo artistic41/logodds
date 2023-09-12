0dbd1f77288e5305c9fdb727138621893a716383
artistic software <artisticsoftware41@gmail.com>
2023-09-12 05:06:49 +0000

    ...

---
+++ 0913/1.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio' =>  [[2, 8, 4, 3, 9]] ,
+		'inters' =>  [[2, 8, 4, 3]] ,
+		'Favorite' =>  '2',
+		'Red QPL values'      =>  '9, 3',
+		'Black QPL values'    =>  '2, 8, 4',
+		'Inter QPL' =>  '2, 8, 4, 3, 9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [[6]] ,
+		'qpl/trio' =>  [[5, 1, 3, 8, 6]] ,
+		'inters' =>  [[5, 1, 3, 8]] ,
+		'Favorite' =>  '5',
+		'Red QPL values'      =>  '5, 1, 3',
+		'Black QPL values'    =>  '6, 8',
+		'Inter QPL' =>  '5, 1, 3, 8, 6',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [[5]] ,
+		'qpl/trio' =>  [[4, 2, 10, 1, 5]] ,
+		'inters' =>  [[4, 2, 10, 1]] ,
+		'Favorite' =>  '4',
+		'Red QPL values'      =>  '1, 5',
+		'Black QPL values'    =>  '4, 2, 10',
+		'Inter QPL' =>  '4, 2, 10, 1, 5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[7, 6]] ,
+		'qpl/trio' =>  [[1, 5, 2, 7, 6]] ,
+		'inters' =>  [[1, 5, 2]] ,
+		'Favorite' =>  '1',
+		'Red QPL values'      =>  '1, 5, 7',
+		'Black QPL values'    =>  '2, 6',
+		'Inter QPL' =>  '1, 5, 2, 7, 6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [[5]] ,
+		'qpl/trio' =>  [[6, 2, 4, 9, 5]] ,
+		'inters' =>  [[6, 2, 4, 9]] ,
+		'Favorite' =>  '6',
+		'Red QPL values'      =>  '9, 5',
+		'Black QPL values'    =>  '6, 2, 4',
+		'Inter QPL' =>  '6, 2, 4, 9, 5',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [[10, 4, 11, 7, 5]] ,
+		'inters' =>  [[10, 4, 11, 7, 5]] ,
+		'Favorite' =>  '10',
+		'Red QPL values'      =>  '7, 5',
+		'Black QPL values'    =>  '10, 4, 11',
+		'Inter QPL' =>  '10, 4, 11, 7, 5',
+	],
+];
---
+++ 0913/2.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '2',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '5',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '4',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[7, 6]] ,
+		'qpl/trio' =>  [[1, 5, 2, 7, 6]] ,
+		'inters' =>  [[1, 5, 2]] ,
+		'Favorite' =>  '1',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '6, 7',
+		'Place values'    =>  '1, 5, 2',
+		'Red QPL values'      =>  '1, 5, 7',
+		'Black QPL values'    =>  '2, 6',
+		'Inter Inters' =>  '1, 5, 2',
+		'Inter QPL' =>  '1, 5, 2, 7, 6',
+		'I' =>  '',
+		'Bet' =>  '6, 7',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '6',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [] ,
+		'qpl/trio' =>  [] ,
+		'inters' =>  [] ,
+		'Favorite' =>  '10',
+		'Sure Place'    =>  '',
+		'All Wins values'    =>  '',
+		'Place values'    =>  '',
+		'Red QPL values'      =>  '',
+		'Black QPL values'    =>  '',
+		'Inter Inters' =>  '',
+		'Inter QPL' =>  '',
+		'I' =>  '',
+		'Bet' =>  '',
+	],
+];

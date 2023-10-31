5076d8cf45ce2e601a407aae338922b49e416f03
artistic software <artisticsoftware41@gmail.com>
2023-10-31 06:16:25 +0000

    ...

---
+++ 1101/1.php
@@ -0,0 +1,118 @@
+<?php
+
+return [
+	'2' => [
+		/**
+		Race 2
+		*/
+		'wins' =>  [[8, 7]] ,
+		'qpl/trio'       =>  [[6, 11, 1, 8, 7]] ,
+		'new 2 qpl/trio' =>  [[6, 11, 1, 8, 7]] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '6, 11, 1, 8, 7',
+		'New 2 QPL values'  =>  '6, 11, 1, 8, 7',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '6',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '6, 11, 1',
+		'Hedge Place' =>  '9, 4',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio'       =>  [[1, 12, 10, 6, 9]] ,
+		'new 2 qpl/trio' =>  [] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '1, 10, 12, 6, 9',
+		'New 2 QPL values'  =>  '',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '1',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '1, 10, 12, 6',
+		'Hedge Place' =>  '8, 3',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'wins' =>  [[9]] ,
+		'qpl/trio'       =>  [[8, 4, 6, 12, 9]] ,
+		'new 2 qpl/trio' =>  [] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '8, 9, 4, 12, 6',
+		'New 2 QPL values'  =>  '',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '8',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '8, 4, 12, 6',
+		'Hedge Place' =>  '2, 5',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'wins' =>  [[5]] ,
+		'qpl/trio'       =>  [[3, 1, 4, 8, 5]] ,
+		'new 2 qpl/trio' =>  [] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '3, 1, 5, 4, 8',
+		'New 2 QPL values'  =>  '',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '3',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '3, 1, 4, 8',
+		'Hedge Place' =>  '12, 7',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'wins' =>  [[2]] ,
+		'qpl/trio'       =>  [[3, 1, 12, 4, 2]] ,
+		'new 2 qpl/trio' =>  [] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '3, 4, 2, 1, 12',
+		'New 2 QPL values'  =>  '',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '3',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '3, 4, 1, 12',
+		'Hedge Place' =>  '11, 8',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'wins' =>  [[2, 7]] ,
+		'qpl/trio'       =>  [[4, 6, 3, 2, 7]] ,
+		'new 2 qpl/trio' =>  [[4, 6, 3, 2, 7]] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '2, 4, 7, 6, 3',
+		'New 2 QPL values'  =>  '2, 4, 7, 6, 3',
+		'New 3 QPL values'  =>  '',
+		//Primes majority,
+		'primes' =>  '2, 7, 3',
+		'Place' =>  '2',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '4, 6, 3',
+		'Hedge Place' =>  '11, 12',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'wins' =>  [[10, 4]] ,
+		'qpl/trio'       =>  [[3, 1, 12, 10, 4]] ,
+		'new 2 qpl/trio' =>  [[3, 1, 12, 10, 4]] ,
+		'new 3 qpl/trio' =>  [] ,
+		'All QPL values'    =>  '3, 1, 12, 10, 4',
+		'New 2 QPL values'  =>  '3, 1, 12, 10, 4',
+		'New 3 QPL values'  =>  '',
+		'Place' =>  '3',
+		//HEDGE BETS,
+		'Trio/Qin' =>  '3, 1, 12',
+		'Hedge Place' =>  '9, 6',
+	],
+];

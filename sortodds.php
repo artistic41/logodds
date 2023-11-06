b45c8eb6fe7ae916af4bf30aaf02e4b4a7e99e30
artistic software <artisticsoftware41@gmail.com>
2023-11-06 03:58:49 +0000

    ...

--- sortodds.php
+++ sortodds.php
@@ -179,32 +179,6 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
     $allQplValues = array_keys($qplsOdds);
     $first1 = $allQplValues[0];
 
-    //keep only the trios that contain wins containing exactly two elements
-    $new2Trios = [];
-    $new3Trios = [];
-    foreach($wins as $winsItem){
-        if(count($winsItem) == 2){
-            $itemValues = array_values($winsItem);
-            foreach($qplTrios as $qplItem){
-                if(in_array($itemValues[0], $qplItem) && in_array($itemValues[1], $qplItem)){
-                    if(!in_my_array($qplItem, $new2Trios)){
-                        $new2Trios[] = $qplItem;
-                    }
-                }
-            }
-        }
-        if(count($winsItem) == 3){
-            $itemValues = array_values($winsItem);
-            foreach($qplTrios as $qplItem){
-                if(in_array($itemValues[0], $qplItem) && in_array($itemValues[1], $qplItem) && in_array($itemValues[2], $qplItem)){
-                    if(!in_my_array($qplItem, $new3Trios)){
-                        $new3Trios[] = $qplItem;
-                    }
-                }
-            }
-        }
-    }
-
     $showRace = true;
 
     //Sort  allWinsValues by odds
@@ -218,82 +192,26 @@ for ($raceNumber = 1; $raceNumber <= $totalRaces; $raceNumber++) {
 
     $racetext .= "\t\t'wins' =>  $WINSText ,\n";
     $racetext .= "\t\t'qpl/trio'       =>  $QPLText ,\n";
-    $racetext .= "\t\t'All QPL values'    =>  '" . implode(", ", $allQplValues).  "',\n";
-    $racetext .= "\t\t'All Runners   '    =>  '" . implode(", ", $runners).  "',\n";
+    $racetext .= "\t\t'All Runners   '  =>  '" . implode(", ", $runners).  "',\n";
+    $racetext .= "\t\t'All QPL values'  =>  '" . implode(", ", $allQplValues).  "',\n";
+    $racetext .= "\t\t'All Wins'        =>  '" . implode(", ", $allWinsValues).  "',\n";
+    $racetext .= "\t\t//Count(Wins)     =  " . count($wins).  ",\n";
+    $racetext .= "\t\t//Count(QPL/Trio) =  " . count($qplTrios).  ",\n";
 
-    $tce = array_slice($runners, 0, 6);
-    $whatever = array_slice($allQplValues, 0, 6);
-    $diff1 = array_diff($tce, $whatever);
-    $diff2 = array_diff($whatever, $tce);
+    $tce1 = array_slice($allQplValues, 0, 6);
+    $tce2 = array_slice($runners, 0, 6);
 
-    $forReference = array_diff($allQplValues, $allWinsValues);
-    $weird = array_diff($runners, $allQplValues);
-    $weird = array_values($weird);
-    //1. Find those in allQplValues that have lower odds than weird[0]
-    if(!empty($weird)){
-        $mySet1 = [];
-        foreach($allQplValues as $value){
-            if($allOdds[$raceNumber][$value] >= $allOdds[$raceNumber][$weird[0]]){
-                $mySet1[] = $value;
-            }
-        }
-        if(!empty(array_intersect($mySet1, $forReference)) 
-            && !in_array(1, $forReference)
-            && count($forReference) > 3 
-            && in_array($first1, $forReference) 
-            && $first1 != 1)
-        {
-                $WP = $first1;
-        }
-    }
-    //2. Find those in weird that have better odds than end($allQplValues)
-    $mySet2 = [];
-    foreach($weird as $value){
-        if($allOdds[$raceNumber][$value] <= $allOdds[$raceNumber][end($allQplValues)]){
-            $mySet2[] = $value;
-        }
-    }
+    $diff1 = array_diff($allWinsValues, $tce1);
+    $diff2 = array_diff($tce1, $allWinsValues);
     
-    if(!empty($allWinsValues)){
-        $lowerBound = $allWinsValues[0];
-        $higherBound = $allWinsValues[count($allWinsValues) - 1];
-        $smallSet = [];
-        $mediumSet= [];
-        $bigSet = [];
-
-        foreach($allQplValues as $putain){
-            if($allOdds[$raceNumber][$putain] < $allOdds[$raceNumber][$lowerBound]) $smallSet[] = $putain;
-            if($allOdds[$raceNumber][$putain] > $allOdds[$raceNumber][$lowerBound] 
-                && $allOdds[$raceNumber][$putain] < $allOdds[$raceNumber][$higherBound]
-                && !in_array($putain, $allWinsValues)) {
-                    $mediumSet[] = $putain;
-                }
-            if($allOdds[$raceNumber][$putain] > $allOdds[$raceNumber][$higherBound]) $bigSet[] = $putain;
-        }
-
-        if(count($forReference) >= 4 ){
-            if($first1 != 1 && in_array($first1, $forReference) && count($smallSet) < 3 && !empty($allWinsValues))
-            {
-                $Place = $first1;
-            }
-        }
-        if(!empty($diff1) && (isset($WP) || isset($Place))){
-            if(empty(array_diff($diff1, [1]))){
-                $racetext .= "\t\t'WP' =>  '1',\n";
-            }
-            else{
-                $racetext .= "\t\t'WP' =>  '" . $first1 . "',\n";
-            }
-            $racetext .= "\t\t'diff1' =>  '" . implode(", ", $diff1) . "',\n";
-            $racetext .= "\t\t'diff2' =>  '" . implode(", ", $diff2) . "',\n";
-            $racetext .= "\t\t'Tce' =>  '" . implode(", ", $tce) . "',\n";
-        }
+    if(count($wins) > 2 && count($diff2) >= 4 && in_array($first1, $diff2)){
+        $racetext .= "\t\t'WP' =>  '" . $first1 . "',\n";
     }
+    $racetext .= "\t\t'diff1' =>  '" . implode(", ", $diff1) . "',\n";
+    $racetext .= "\t\t'diff2' =>  '" . implode(", ", $diff2) . "',\n";
     $racetext .= "\t],\n";
     unset($oldWINS);
     unset($oldQPLTrio);
-    unset($WP);
-    unset($Place);
     if($showRace) $outtext .= $racetext;
 }
 

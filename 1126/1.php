2c9ca579c2cc4c4c9e639e7c69f4d7db102f0cd7
artistic software <artisticsoftware41@gmail.com>
2023-11-25 05:37:38 +0000

    ...

---
+++ 1126/1.php
@@ -0,0 +1,97 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'All Runners   '  =>  '6, 14, 10, 3, 9, 11, 2, 1, 13, 8, 4, 7, 5, 12',
+		'Total Runners '  =>  14,
+		//In first 6 runners!
+		'places' => '14, 6, 10',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'All Runners   '  =>  '2, 7, 1, 11, 10, 5, 3, 4, 9, 13, 12, 8, 6, 14',
+		'Total Runners '  =>  14,
+		'WP' => '2',
+		'places' => '6, 14',
+		'places4' => '6, 14',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'All Runners   '  =>  '4, 2, 7, 11, 3, 6, 1, 9, 5, 8, 12, 10',
+		'Total Runners '  =>  12,
+		'Maybe WP' => '4',
+		'places' => '8, 12, 10',
+		'places4' => '8, 12, 10',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'All Runners   '  =>  '2, 4, 10, 6, 8, 9, 3, 12, 7, 11, 5, 1',
+		'Total Runners '  =>  12,
+		'places' => '3, 12, 7',
+		'places4' => '3, 12, 7',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'All Runners   '  =>  '3, 14, 10, 12, 1, 13, 2, 5, 7, 6, 9, 4, 8, 11',
+		'Total Runners '  =>  14,
+		//In first 6 runners!
+		'places' => '3, 14, 10',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'All Runners   '  =>  '2, 5, 3, 9, 1, 4, 7, 10, 6, 14, 11, 12, 13, 8',
+		'Total Runners '  =>  14,
+		'places' => '6, 14, 11',
+		'places4' => '6, 14, 11',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'All Runners   '  =>  '1, 5, 11, 10, 2, 9, 6, 8, 7, 3, 4',
+		'Total Runners '  =>  11,
+		//In first 6 runners!
+		'places' => '5, 11, 10',
+		'places4' => '10',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'All Runners   '  =>  '4, 2, 9, 6, 1, 8, 10, 11, 3, 7, 5',
+		'Total Runners '  =>  11,
+		'places' => '10, 11, 3',
+		'places4' => '10, 11, 3',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'All Runners   '  =>  '10, 7, 2, 8, 4, 6, 5, 1, 12, 14, 11, 13, 3, 9',
+		'Total Runners '  =>  14,
+		'places' => '12, 14, 11',
+		'places4' => '12, 14, 11',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'All Runners   '  =>  '10, 1, 9, 5, 2, 4, 7, 12, 3, 11, 6, 8',
+		'Total Runners '  =>  12,
+		'places' => '12, 7, 3',
+		'places4' => '7, 12, 3',
+	],
+];

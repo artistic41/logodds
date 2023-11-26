76b1ef20e0220c758b20a548e43f953156c6014b
artistic software <artisticsoftware41@gmail.com>
2023-11-26 04:16:48 +0000

    ...

--- 1126/1.php
+++ 1126/1.php
@@ -5,49 +5,50 @@ return [
 		/**
 		Race 1
 		*/
-		'All Runners   '  =>  '14, 6, 10, 3, 9, 2, 11, 1, 13, 8, 5, 7, 4, 12',
+		'All Runners   '  =>  '14, 3, 10, 6, 1, 9, 11, 2, 13, 8, 7, 5, 4, 12',
 		'Total Runners '  =>  14,
-		'places' => '14, 6, 10',
+		'places' => '14, 10, 6',
+		'places4' => '6',
 	],
 	'2' => [
 		/**
 		Race 2
 		*/
-		'All Runners   '  =>  '2, 7, 11, 1, 4, 3, 10, 9, 6, 5, 13, 8, 14, 12',
+		'All Runners   '  =>  '2, 7, 1, 11, 4, 5, 6, 3, 9, 10, 13, 8, 14, 12',
 		'Total Runners '  =>  14,
 		'places' => '6, 5, 8, 14, 12',
-		'places4' => '6, 5, 8, 14, 12',
+		'places4' => '5, 6, 8, 14, 12',
+		'candidate(k = 10, posK = 9)' => '4',
+		'candidate(k = 9, posK = 8)' => '5',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'All Runners   '  =>  '4, 2, 7, 3, 11, 6, 9, 1, 5, 10, 8, 12',
+		'All Runners   '  =>  '4, 2, 11, 3, 6, 7, 5, 9, 1, 10, 8, 12',
 		'Total Runners '  =>  12,
 		'WP' => '4',
 		'places' => '10, 8, 12',
 		'places4' => '10, 8, 12',
 		'candidate(k = 12, posK = 11)' => '4',
-		'candidate(k = 10, posK = 9)' => '7',
-		'candidate(k = 6, posK = 5)' => '9',
+		'candidate(k = 10, posK = 9)' => '11',
 		'candidate(k = 2, posK = 1)' => '8',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'All Runners   '  =>  '4, 2, 6, 8, 3, 10, 7, 9, 1, 12, 5, 11',
+		'All Runners   '  =>  '4, 2, 6, 8, 3, 7, 10, 12, 9, 1, 5, 11',
 		'Total Runners '  =>  12,
-		'places' => '3, 7, 9, 12, 1, 11, 5',
-		'places4' => '3, 7, 9, 1, 12, 5, 11',
-		'candidate(k = 7, posK = 6)' => '10',
+		'places' => '3, 7, 10, 12, 9, 1, 5, 11',
+		'places4' => '3, 7, 10, 12, 9, 1, 5, 11',
 		'candidate(k = 2, posK = 1)' => '5',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'All Runners   '  =>  '3, 14, 10, 12, 7, 2, 13, 1, 5, 6, 9, 11, 4, 8',
+		'All Runners   '  =>  '3, 14, 10, 12, 7, 2, 5, 13, 1, 6, 9, 11, 4, 8',
 		'Total Runners '  =>  14,
 		//In first 6 runners!
 		'places' => '3, 14, 10, 12',
@@ -57,32 +58,31 @@ return [
 		/**
 		Race 6
 		*/
-		'All Runners   '  =>  '2, 5, 3, 9, 7, 1, 4, 6, 11, 10, 14, 12, 13, 8',
+		'All Runners   '  =>  '2, 5, 1, 3, 9, 7, 4, 11, 6, 10, 14, 12, 13, 8',
 		'Total Runners '  =>  14,
-		'places' => '6, 11, 10, 14, 12, 13',
-		'places4' => '6, 11, 10, 14, 12, 13',
-		'candidate(k = 10, posK = 9)' => '7',
-		'candidate(k = 3, posK = 2)' => '12',
+		'places' => '11, 6, 10, 14, 12, 13',
+		'places4' => '11, 6, 10, 14, 12, 13',
+		'candidate(k = 10, posK = 9)' => '9',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
-		'All Runners   '  =>  '5, 11, 1, 10, 9, 2, 6, 7, 4, 8, 3',
+		'All Runners   '  =>  '5, 1, 11, 10, 2, 9, 6, 7, 3, 8, 4',
 		'Total Runners '  =>  11,
 		//In first 6 runners!
-		'places' => '5, 11, 1, 10',
+		'places' => '5, 1, 11, 10',
 		'places4' => '10',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
-		'All Runners   '  =>  '2, 4, 9, 10, 1, 6, 11, 8, 3, 7, 5',
+		'All Runners   '  =>  '2, 4, 9, 1, 10, 11, 6, 8, 3, 7, 5',
 		'Total Runners '  =>  11,
-		'places' => '10, 6, 11, 8, 3',
-		'places4' => '10, 6, 11, 8, 3',
-		'candidate(k = 6, posK = 5)' => '6',
+		//In first 6 runners!
+		'places' => '10, 11, 6, 8, 3',
+		'places4' => '10, 11, 6, 8, 3',
 	],
 	'9' => [
 		/**
@@ -97,12 +97,12 @@ return [
 		/**
 		Race 10
 		*/
-		'All Runners   '  =>  '10, 9, 1, 7, 4, 5, 2, 3, 6, 8, 11, 12',
+		'All Runners   '  =>  '10, 9, 1, 4, 7, 5, 2, 3, 6, 11, 8, 12',
 		'Total Runners '  =>  12,
 		'WP' => '10',
-		'places' => '7, 2, 3, 11, 12',
-		'places4' => '7, 2, 3, 11, 12',
+		'places' => '7, 2, 3, 11, 8, 12',
+		'places4' => '7, 2, 3, 11, 8, 12',
 		'candidate(k = 12, posK = 11)' => '10',
-		'candidate(k = 11, posK = 10)' => '9',
+		'candidate(k = 4, posK = 3)' => '6',
 	],
 ];
